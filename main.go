b21c35c7951ea21da1fdf64ba07c47f929924172
priyambodo <maspriyambodo@gmail.com>
2025-08-12 11:57:22 +0700

    update main

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"ternago/backend-api/models"
 	"ternago/backend-api/routes"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	// Create Gin router
 	r := gin.Default()
 
